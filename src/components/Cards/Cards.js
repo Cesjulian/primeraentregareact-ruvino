1427e7becb9ec7c7810d78bef8c1c589b064630b
cesarruv <cesaryjoni@gmail.com>
2022-12-26 21:47:55 -0300

    actualizacion proyecto

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -14,7 +14,6 @@ const Cards = (props) => {
             try {
                 const { data } = await axios.get(`data.json`)
                 setData(data)
-                console.log(data)
             } catch (error) {
                 console.log("error")
             }
@@ -27,15 +26,23 @@ const Cards = (props) => {
     }, []);
 
     return (
-        
-        <div className="card col-sm-12 col-lg-3 m-3" style={{ width: "18rem" }}>
-            <img className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">$</h5>
-                <h6 className="card-title">marca: $</h6>
-                <p className="card-text">$$</p>
-                <a id="botonAccion" className="btn btnColor" onClick="agregado(${indice})">Comprar</a>
-            </div>
+
+        <div className="row justify-content-center">
+            {data.map(
+                ({ nombre, precio, marca, imagen, id , categoria}, indice) => (
+                    <div className="card col-sm-12 col-lg-3 m-3" style={{ width: "18rem" }}>
+                        <img className="card-img-top" src={imagen} alt="..." />
+                        <div className="card-body">
+                            <h5 className="card-title">{nombre}</h5>
+                            <h6 className="card-title">marca:{marca}</h6>
+                            <h6 className="card-title">id del articulo:{id}</h6>
+                            <h6 className="card-title">categoria:{categoria}</h6>
+                            <p className="card-text">${precio}</p>
+                            <a id="botonAccion" className="btn btnColor" onClick="agregado({indice})">Comprar</a>
+                        </div>
+                    </div>
+                )
+            )}
         </div>
     );
 
